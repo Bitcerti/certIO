4d4bd74e5fe7d5008fc57ed6c7e3563b05696d73
Maek Twain <saranshisat@icloud.com>
2018-05-14 10:38:58 +0530

    User, Profile Routing

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,17 +2,17 @@ from django.conf.urls import url, include
 
 from rest_framework import routers
 
-from api.views import user
+from .views.user import UserView
+from .views.profile import ProfileView
 
 routers = routers.DefaultRouter()
-
-routers.register(r'project', user.UserViewSet)
-
 
 
 urlpatterns = [
 
-    url(r'^', include(routers.urls)),
+    url(r'^profiles$', ProfileView.as_view()),
 
+    # Users
+    url(r'^users$', UserView.as_view()),
 ]
 
--- project/api/views/user.py
+++ project/api/views/user.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.serializers.user import UserSerializer
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserView(APIView):
+    @staticmethod
+    def get(request):
+        """
+        List users
+        """
 
-    class Meta:
-        model = User
-        exclude = ('password',)
+        users = User.objects.all()
+        return Response(UserSerializer(users, many=True).data)
